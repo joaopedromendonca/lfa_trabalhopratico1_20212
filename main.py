6e55826acb1035103495e7ff20a90a686a9cb836
JP-Mendonca <joaoooh@gmail.com>
2021-09-28 02:07:09 -0300

    Criação do automato finalizada

--- main.py
+++ main.py
@@ -3,29 +3,59 @@
 
 class Estado:
 
-    def __init__(self, nome: str, ehFinal: bool, ehInicial: bool, transicoes: list) -> None:
+    def __init__(self, nome: str) -> None:
         self.nome = nome
-        self.ehFinal = ehFinal
-        self.ehInicial = ehInicial
-        self.transicoes = transicoes
+        self.ehFinal = False
+        self.ehInicial = False
+        self.transicoes = []
 
 class Automato:
 
-    def __init__(self, estados: set, inicial: str, final: list, alfabeto: set, transicoes: list) -> None:
+    def __init__(self, estados: list, inicial: Estado, final: list, alfabeto: list, transicoes: list) -> None:
         self.estados = estados
-        
         self.inicial = inicial
         self.final = final
         self.alfabeto = alfabeto
         self.transicoes = transicoes
 
+
+def cria_auto(auto_dict):
+
+    estados = []
+    inicial = []
+    final = []
+    alfabeto = set(auto_dict['#alphabet'])
+    transicoes = []
+
+    for s in auto_dict['#states']:
+        estados.append(Estado(s))
+
+    print([e.nome for e in estados])
+
+    for e in estados:
+        if e.nome == auto_dict['#initial']:
+            e.ehInicial = True
+            inicial.append(e)
+        if e.nome in auto_dict['#accepting']:
+            e.ehFinal = True
+            final.append(e)
+        for t in auto_dict['#transitions']:
+            if e.nome == t.split(':')[0]:
+                e.transicoes.append(t)
+                transicoes.append(t)
+
+
+    # auto = Automato(set(auto_dict['#states']), auto_dict['#initial'], auto_dict['#accepting'], set(auto_dict['#alphabet']), auto_dict['#transitions'])
+    auto = Automato(estados,inicial,final,alfabeto,transicoes)
+    print(auto.__repr__)
+
+    return auto
+
 # função que formata os dados do arquivo
 def formata(lista):
 
     rotulos = ['#states','#initial','#accepting','#alphabet','#transitions']
-
     auto_dict = {}
-
     rotulo_atual = ''
 
     for e in lista:
@@ -34,22 +64,20 @@ def formata(lista):
             auto_dict[rotulo_atual] = []
         else:
             auto_dict[rotulo_atual].append(e)
-
     print(auto_dict)
+
     return auto_dict
+
 
 def main():
 
     with open("input.txt") as file:
-        # print(file.readlines())
-
+        # lê input
         auto = file.readlines()
-        print(auto)
-        auto_frmt = [x[:-1] for x in auto]
-        print(auto_frmt)
-        # c = filter(lambda x:x[0]!='#', auto_frmt)
-        r = formata(auto_frmt)
-        
+        # formata input
+        temp = [x[:-1] for x in auto]
+        auto_dict = formata(temp)
+        auto = cria_auto(auto_dict)
 
 
 if __name__ == "__main__":
