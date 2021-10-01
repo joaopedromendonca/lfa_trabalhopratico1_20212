c3dc638e95b0eccc2491d2d9e85abc7491cf2878
JP-Mendonca <joaoooh@gmail.com>
2021-10-01 17:13:33 -0300

    Melhorias na formatação e estruturação do autômato

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 '''
 '''
-
+import re
 class Estado:
 
     def __init__(self, nome: str) -> None:
         self.nome = nome
         self.ehFinal = False
         self.ehInicial = False
-        self.transicoes = []
+        self.transicoes = {}
 
     def __repr__(self) -> str:
         rep = 'Estado(' + self.nome + ')' + str(self.transicoes)
@@ -15,16 +15,26 @@ class Estado:
 
 class Automato:
 
-    def __init__(self, estados: list, inicial: Estado, final: list, alfabeto: list, transicoes: list) -> None:
+    def __init__(self, estados: list, inicial: Estado, final: list, alfabeto: set) -> None:
         self.estados = estados
         self.inicial = inicial
         self.final = final
         self.alfabeto = alfabeto
-        self.transicoes = transicoes
-
+        
     def imprime_auto(self):
         for e in self.estados:
             print(repr(e))
+
+    def valida_palavra(self, palavra: str) -> bool:
+        
+        estado_atual = self.inicial
+        l_palavra = list(palavra)
+        
+        for c in l_palavra:
+            if c not in self.alfabeto:
+                return False
+            if estado_atual.transicoes == 1:
+                pass
 
 
 def cria_auto(auto_dict):
@@ -34,10 +44,26 @@ def cria_auto(auto_dict):
     inicial = Estado('temp')
     final = []
     alfabeto = set(auto_dict['#alphabet'])
-    transicoes = []
+    transicoes = {}
 
     for s in auto_dict['#states']:
         estados.append(Estado(s))
+
+    for t in auto_dict['#transitions']:
+        format_t = re.split(':|\>', t)
+        print(format_t)
+        nome = format_t[0]
+
+        ''' 
+        as transições são formatadas de modo que a chave seja o nome do estado, 
+        o primeiro elemento da tupla o caractere da transição e o(s) estados 
+        sejam o segundo elemento da tupla
+        '''
+        
+        if nome in transicoes:
+            transicoes[nome].append((format_t[1],format_t[2]))
+        else:
+            transicoes[nome] = [(format_t[1],format_t[2])]
 
     for e in estados:
         if e.nome == auto_dict['#initial']:
@@ -46,17 +72,14 @@ def cria_auto(auto_dict):
         if e.nome in auto_dict['#accepting']:
             e.ehFinal = True
             final.append(e)
-        for t in auto_dict['#transitions']:
-            if e.nome == t.split(':')[0]:
-                e.transicoes.append(t)
-                transicoes.append(t)
+        e.transicoes = transicoes[e.nome]
     
-    auto = Automato(estados,inicial,final,alfabeto,transicoes)
+    auto = Automato(estados,inicial,final,alfabeto)
 
     return auto
 
 # função que formata os dados do arquivo
-def formata(lista):
+def formata_arquivo(lista):
 
     rotulos = ['#states','#initial','#accepting','#alphabet','#transitions']
     auto_dict = {}
@@ -78,8 +101,9 @@ def main():
         # lê input
         auto = file.readlines()
         # formata input
-        temp = [x[:-1] for x in auto]
-        auto_dict = formata(temp)
+        temp = [x.rstrip() for x in auto]
+        auto_dict = formata_arquivo(temp)
+        print(auto_dict)
         auto = cria_auto(auto_dict)
         auto.imprime_auto()
 
